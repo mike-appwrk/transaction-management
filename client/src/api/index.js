f6b9454a783c48a8fcf04bd6e4e130eac05669e5
Mike S <mike.s@appwrk.com>
2022-12-28 16:41:21 +0530

    enable transaction creation from frontend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,3 +2,11 @@ const url = "http://localhost:7777/api";
 
 
 export const getTransactions = () => fetch(url).then(res => res.json()).catch(console.log);
+
+export const createTransaction = (transaction) => fetch(`${url}/create`, {
+  method: 'POST', 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(transaction),
+});
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,8 +1,81 @@
+import { useState } from "react";
+import { createTransaction } from "../api";
+import { useNavigate } from "react-router-dom";
+
 function Form () {
+
+  const [transaction, setTransaction] = useState({
+    description: '',
+    amount: 0,
+    type: 'credit',
+    date: ''
+  });
+
+  const navigate = useNavigate();
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+
+    async function postTransaction() {
+      try {
+        const transactionRes = await createTransaction(transaction);
+        console.log({ transactionRes })
+        return navigate("/");
+      } catch (error) {
+        console.log({message: error.message});
+      }
+    }
+
+    postTransaction();
+
+  }
+
+  function handleChange(e) {
+    setTransaction({
+      ...transaction,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  function handleClear() {
+    setTransaction({
+      description: '',
+      amount: 0,
+      type: 'credit',
+      date: ''
+    });
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleFormSubmit}>
 
+        <div className="form-group">
+          <label htmlFor="type">Transaction Type</label>
+          <select id="type" name="type" value={transaction.type} onChange={handleChange}>
+            <option value="credit">Credit</option>
+            <option value="debit">Debit</option>
+          </select>
+        </div>
+        
+        <div className="form-group">
+          <label htmlFor="amount">Amount</label>
+          <input type="number" id="amount" name="amount" value={transaction.amount} onChange={handleChange}/>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="description">Description</label>
+          <input type="text" id="description" name="description" value={transaction.description} onChange={handleChange}/>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="date">Date</label>
+          <input type="date" id="date" name="date" value={transaction.date} onChange={handleChange}/>
+        </div>
+
+        <button type="submit">Submit</button>
+        
+        <button type="button" onClick={handleClear}>Clear</button>
       </form>
     </div>
   )
